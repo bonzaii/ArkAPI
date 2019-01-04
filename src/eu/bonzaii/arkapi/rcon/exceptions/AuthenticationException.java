7ba48963ab7cf303145a067fc74898e157604e5f
Bonzaii <thebonzaii@gmail.com>
2019-01-04 22:46:40 +0000

    A lot of project setting changes + adding maven framework. Will not use YAML instead of JSON.

--- src/eu/bonzaii/arkapi/rcon/exceptions/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package eu.bonzaii.arkapi.rcon.exceptions;
-
-public class AuthenticationException extends Exception {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
--- src/eu/bonzaii/arkapi/rcon/exceptions/MalformedPacketException.java
+++
@@ -1,9 +0,0 @@
-package eu.bonzaii.arkapi.rcon.exceptions;
-
-import java.io.IOException;
-
-public class MalformedPacketException extends IOException {
-    public MalformedPacketException(String message) {
-        super(message);
-    }
-}
